21ff6e3d23a66aba957f1a1551883f73028052b4
nargok <nargok@gmail.com>
2023-11-28 07:58:23 +0900

    Footer

---
+++ src/app/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='py-2 px-4 border-t'>
+      <small>@2023 nargok</small>
+    </footer>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './Header'
+import Footer from './Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className='container mx-auto bg-slate-700 text-slate-50'>
         <Header />
         {children}
-        <footer>Footer</footer>
+        <Footer />
       </body>
     </html>
   )
