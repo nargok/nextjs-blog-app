d7d97989a32bf86f6dd2c4a6a30d2f0325901d43
nargok <nargok@gmail.com>
2023-12-10 13:35:04 +0900

    Error Page

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -3,6 +3,10 @@ import { Article } from './types'
 export const getArticles = async (): Promise<Article[]> => {
   const res = await fetch(`http://localhost:3001/posts`, { cache: 'no-store' }) // SSR since Article will be post 2 or 3 times per a day
 
+  if (!res.ok) {
+    throw new Error('Error')
+  }
+
   const articles = await res.json()
   return articles
 }
