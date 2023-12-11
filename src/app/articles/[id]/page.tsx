9e1fb171bef7282d2f11f0a32e5e55e28d6d6ca1
nargok <nargok@gmail.com>
2023-12-11 22:29:47 +0900

    display article data from API

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -4,14 +4,13 @@ import { getDetailArticle } from '@/blogAPI'
 
 export default async function Page({ params }: { params: { id: string } }) {
   const article = await getDetailArticle(params.id)
-  console.log(article)
 
   return (
     <div className='max-w-3xl mx-auto p-5'>
       <Image src='https://source.unsplash.com/random' alt='' width={1280} height={300} />
-      <h1 className='text-4xl text-center mb-10 mt-10'>Next.js14の勉強中です</h1>
+      <h1 className='text-4xl text-center mb-10 mt-10'>{article.title}</h1>
       <div className='text-lg leading-relaxed text-justify'>
-        <p>ここが本文です</p>
+        <p>{article.content}</p>
       </div>
     </div>
   )
